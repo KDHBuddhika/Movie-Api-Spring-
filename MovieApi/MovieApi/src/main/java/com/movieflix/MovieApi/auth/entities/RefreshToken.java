d658dd5061c890c3b19f1be5b4e62af2723f3c89
KDHBuddhika <136336705+KDHBuddhika@users.noreply.github.com>
2025-04-22 18:38:50 +0530

    JWT service , refresh token service

--- MovieApi/MovieApi/src/main/java/com/movieflix/MovieApi/auth/entities/RefreshToken.java
+++ MovieApi/MovieApi/src/main/java/com/movieflix/MovieApi/auth/entities/RefreshToken.java
@@ -2,10 +2,18 @@ package com.movieflix.MovieApi.auth.entities;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class RefreshToken {
     
     @Id
---
+++ MovieApi/MovieApi/src/main/java/com/movieflix/MovieApi/auth/services/AuthFilterService.java
@@ -0,0 +1,7 @@
+package com.movieflix.MovieApi.auth.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthFilterService {
+}
